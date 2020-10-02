7beca6b52d02cc0fe936c8f823bc7d4ae504f3f8
Mark Galang <galangmarkg@gmail.com>
2020-10-02 11:30:10 -0400

    Add Form action

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,6 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import Alert from "@material-ui/lab/Alert";
+import Collapse from "@material-ui/core/Collapse";
 
 const ContactFrom = () => {
+  const [serverState, setServerState] = useState({
+    submitting: false,
+    status: null,
+  });
+
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleServerResponse = (ok, msg, form) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg },
+    });
+    setShowAlert(true);
+    if (ok) {
+      form.reset();
+    }
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    setServerState({ submitting: true });
+    axios({
+      method: "post",
+      url: "https://formspree.io/f/mdopkjro",
+      data: new FormData(form),
+    })
+      .then(() => {
+        handleServerResponse(true, "Thanks!", form);
+      })
+      .catch(({ response }) => {
+        handleServerResponse(false, response.data.error, form);
+      });
+
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 4000);
+  };
+
+  const alertMessage = () => {
+    if (serverState.status.ok) {
+      return (
+        <Alert onClose={() => setShowAlert(false)} severity="success">
+          Awesome! I'll email you back as soon as possible.
+        </Alert>
+      );
+    } else {
+      switch (serverState.status.msg) {
+        case "Validation errors":
+          return (
+            <Alert onClose={() => setShowAlert(false)} severity="error">
+              Oops! Please enter a valid email address so I can email you back.
+            </Alert>
+          );
+        default:
+          return (
+            <Alert onClose={() => setShowAlert(false)} severity="error">
+              Something went wrong. Please try again.
+            </Alert>
+          );
+      }
+    }
+  };
+
   return (
     <div className="contact-form">
       <p className="contact-form__description">
@@ -9,27 +76,41 @@ const ContactFrom = () => {
         social media pages
       </p>
 
-      <form action="#" className="contact-form__form">
+      <Collapse in={showAlert}>{serverState.status && alertMessage()}</Collapse>
+
+      <form
+        onSubmit={handleOnSubmit}
+        method="POST"
+        className="contact-form__form"
+      >
         <input
           type="text"
           name="name"
+          required
           placeholder="Your Name here"
           className="contact-form__input"
         />
         <input
-          type="text"
+          type="email"
           name="email"
+          required
           placeholder="Your Email here"
           className="contact-form__input"
         />
         <textarea
-          type="text"
           rows={5}
           name="message"
+          required
           placeholder="Your Message here"
           className="contact-form__input contact-form__input--textarea"
         ></textarea>
-        <button className="button button--gold u-margin-y-sm" type="submit">
+        <button
+          disabled={serverState.submitting}
+          className={`button button--gold u-margin-y-sm ${
+            serverState.submitting && "button--loading"
+          }`}
+          type="submit"
+        >
           Send Message
         </button>
       </form>
